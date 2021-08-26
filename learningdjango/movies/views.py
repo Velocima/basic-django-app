e73f46f5d3a7656eecd670f0e32d049f945d82ad
Velo <hartley.max@outlook.com>
2021-08-26 14:56:37 +0100

    add route protection to add movie page

--- learningdjango/movies/views.py
+++ learningdjango/movies/views.py
@@ -1,12 +1,15 @@
+from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import AddGenreForm, AddMovieForm
 from .models import Movie
 
+def is_member(user):
+    return user.groups.filter(name='Manager').exists()
 
 # Create your views here.
 def index(request):
   movies = Movie.objects.all()
-  return render(request, 'movies/index.html', context={"movies": movies})
+  return render(request, 'movies/index.html', context={"movies": movies, 'is_manager': is_member(request.user)})
 
 def show(request, id):
   movie = get_object_or_404(Movie, pk=id)
@@ -16,6 +19,8 @@ def about(request):
   return render(request, 'movies/about.html')
 
 def create(request):
+  if not is_member(request.user):
+    return HttpResponseForbidden()
   if request.method == 'POST':
     new_movie_data = AddMovieForm(request.POST)
     if new_movie_data.is_valid():
